470db06a1a065c0d1675ace23448544cb8bab8b3
aerofanz <55221937+aerofanz@users.noreply.github.com>
2020-09-30 15:30:06 +0700

    init curency repo

---
+++ Repositorys/implements/Master/CurrencyRepository.cs
@@ -0,0 +1,90 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+// using Dapper.Bulk;
+using Dapper.Contrib.Extensions;
+using MySambu.Api.Controllers.Master;
+using MySambu.Api.Models.Master;
+using MySambu.Api.Repositorys.Interfaces;
+
+namespace MySambu.Api.Repositorys.implements
+{
+    internal class CurrencyRepository : BaseRepository, ICurrencyRepository
+    {
+        public CurrencyRepository(IDbTransaction transaction) : base(transaction)
+        {
+
+        }
+
+        public Task Delete(Currency obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Currency>> GetAll()
+        {
+            var data = await Connection.GetAllAsync<Currency>(transaction: Transaction);
+            return data;
+        }
+
+        public async Task Save(Currency obj)
+        {
+            await Connection.QueryAsync("pMst_CountrySave", new
+            {
+                CurrencyID = obj.CurrencyId,
+                CurrencyName = obj.CurrencyName,
+                CurrencySymbol = obj.CurrencySymbol,
+                CurrencySayInWords = obj.CurrencySayInWords,
+                CurrencySayInWords2 = obj.CurrencySayInWords2,
+                UserID = obj.CreatedBy,
+                Flag = 0
+            }, commandType: CommandType.StoredProcedure, transaction: Transaction);
+        }
+
+        public Task Update(Currency obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Curremcy Rate
+        public async Task SaveRate(IEnumerable<CurrencyRates> dt)
+        {
+            foreach (var data in dt)
+            {
+                await Connection.InsertAsync<CurrencyRates>(data, transaction: Transaction);
+            }
+        }
+
+        public async Task UpdateRate(IEnumerable<CurrencyRates> dt)
+        {
+            foreach (var data in dt)
+            {
+                await Connection.QueryAsync("UPDATE tMst_CurrencyRate SET CurrencyRate = @l1, LastUpdatedBy = @l2, LastUpdatedDate = @l3@ WHERE CurrencyDate = @l4",
+                        new { l1 = data.CurrencyRate, l2 = data.LastUpdatedBy, l3 = data.LastUpdatedDate, l4 = data.CurrencyDate }, transaction: Transaction);
+            }
+        }
+
+        public async Task<IEnumerable<CurrencyRates>> GetListCurrencyRate()
+        {
+            var data = await Connection.QueryAsync<CurrencyRates>("SELECT * FROM tMst_CurrencyRate");
+            return data;
+        }
+
+        public async Task<IEnumerable<CurrencyRates>> GetListCurrencyRateBydate(System.DateTime tgl)
+        {
+            var data = await Connection.QueryAsync<CurrencyRates>("SELECT * FROM tMst_CurrencyRate where CurrencyDate = @cur ", new { cur = tgl }, transaction: Transaction);
+            return data;
+        }
+
+        public Task<Currency> GetByID(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Delete(string id, string by)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
