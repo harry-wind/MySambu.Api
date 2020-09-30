fd3f62118214a4fafb241b6b22304fa8c4f6a193
aerofanz <55221937+aerofanz@users.noreply.github.com>
2020-09-30 15:31:41 +0700

    init interface role previlege repo

---
+++ Repositorys/Interfaces/Utility/IRolePrevilegeRepository.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MySambu.Api.Models.Utility;
+
+namespace MySambu.Api.Repositorys.Interfaces
+{
+    public interface IRolePrevilegeRepository : IBaseRepository<RolePrevilege>
+    {
+        Task<IEnumerable<RolePrevilege>> GetByID2(string id);
+    }
+}
