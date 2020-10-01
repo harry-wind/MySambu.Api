8fbb7eddd2d582b349d32da69eae489c5e6a719b
aerofanz <55221937+aerofanz@users.noreply.github.com>
2020-10-01 08:59:58 +0700

    init item subcate

---
+++ Models/Master/ItemSubCategory.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace MySambu.Api.Models.Master
+{
+    public class ItemSubCategory
+    {
+        public short SubCategoryID { get; set; }
+        public short RevisionNo { get; set; }
+        public string SubCategoryName { get; set; }
+        public short CategoryID { get; set; }
+        public string ACCID { get; set; }
+        public bool NotActive { get; set; }
+        public short? CompanyID { get; set; }
+        public string CreatedBy { get; set; }
+        public string CreatedByName { get; set; }
+        public string CreatedByPosition { get; set; }
+        public string CreatedByVersion { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastUpdatedBy { get; set; }
+        public string LastUpdatedByName { get; set; }
+        public string LastUpdatedByPosition { get; set; }
+        public string LastUpdatedByVersion { get; set; }
+        public DateTime? LastUpdatedDate { get; set; }
+        public string Computer { get; set; }
+        public DateTime ComputerDate { get; set; }
+
+    }
+}
