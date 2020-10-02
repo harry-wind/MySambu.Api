a5e1d23660f38d198a8f247446647a18ea487d11
aerofanz <55221937+aerofanz@users.noreply.github.com>
2020-10-02 10:12:22 +0700

    update itemuomconve

--- Repositorys/Interfaces/Master/IItemUomConvertionRepository.cs
+++ Repositorys/Interfaces/Master/IItemUomConvertionRepository.cs
@@ -1,6 +1,8 @@
+using MySambu.Api.Models.Master;
+
 namespace MySambu.Api.Repositorys.Interfaces
 {
-    public interface IItemUomConvertionRepository
+    public interface IItemUomConvertionRepository : IBaseRepository<ItemUOMConvertion>
     {
     }
 }
