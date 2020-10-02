f0ec76c68b26793f33baba9ee2217044b27fdcaa
aerofanz <55221937+aerofanz@users.noreply.github.com>
2020-10-02 10:06:23 +0700

    update item repo

--- Repositorys/implements/Master/ItemRepository.cs
+++ Repositorys/implements/Master/ItemRepository.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
 using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using MySambu.Api.Models.Master;
 using MySambu.Api.Repositorys.Interfaces;
 
 namespace MySambu.Api.Repositorys.implements
@@ -10,6 +15,39 @@ namespace MySambu.Api.Repositorys.implements
         public ItemRepository(IDbTransaction transaction) : base(transaction)
         {
             // _transaction = transaction;
+        }
+
+        public Task Delete(Item obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Delete(string id, string by)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Item>> GetAll()
+        {
+            return await Connection.GetAllAsync<Item>(transaction:Transaction);
+        }
+
+        public async Task<Item> GetByID(string id)
+        {
+            return await Connection.QueryFirstOrDefaultAsync<Item>("SELECT * FROM tMst_Item where ItemID = @id", new{id = id}, transaction:Transaction);
+        }
+
+        public async Task Save(Item obj)
+        {
+            await Connection.QueryAsync("pMst_SupplierSave", new
+            {
+                
+            }, commandType: CommandType.StoredProcedure, transaction: Transaction);
+        }
+
+        public Task Update(Item obj)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
