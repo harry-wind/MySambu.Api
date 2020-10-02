385540adfda58f87d966e71f5f0ef5176d0edc83
aerofanz <55221937+aerofanz@users.noreply.github.com>
2020-10-02 10:08:29 +0700

    update item uom convertion repo

--- Repositorys/implements/Master/ItemUomConvertionRepository.cs
+++ Repositorys/implements/Master/ItemUomConvertionRepository.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using MySambu.Api.Models.Master;
 using MySambu.Api.Repositorys.Interfaces;
 
 namespace MySambu.Api.Repositorys.implements
@@ -8,6 +14,38 @@ namespace MySambu.Api.Repositorys.implements
         public ItemUomConvertionRepository(IDbTransaction transaction) : base(transaction)
         {
 
+        }
+
+        public Task Delete(ItemUOMConvertion obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task Delete(string id, string by)
+        {
+           await Connection.QueryAsync("UPDATE tMst_ItemUOMConvertion SET IsActive = false, UpdatedBy = @by, UpdatedDate = @tgl WHERE ID = @id",
+                new{ UpdatedBy = by, UpdatedDate = DateTime.Now, id = id}, transaction:Transaction);
+        }
+
+        public async Task<IEnumerable<ItemUOMConvertion>> GetAll()
+        {
+            return await Connection.GetAllAsync<ItemUOMConvertion>(transaction:Transaction);
+        }
+
+        public async Task<ItemUOMConvertion> GetByID(string id)
+        {
+            return await Connection.QueryFirstOrDefaultAsync<ItemUOMConvertion>("SELECT * FROM tMst_ItemUOMConvertion WHERE ID = @id", new { id = id}, transaction:Transaction);
+        }
+
+        public async Task Save(ItemUOMConvertion obj)
+        {
+            await Connection.InsertAsync<ItemUOMConvertion>(obj, transaction:Transaction);
+        }
+
+        public async Task Update(ItemUOMConvertion obj)
+        {
+            await Connection.QueryAsync("UPDATE tMst_ItemUOMConvertion SET UomUsage = @usage, UomPurchase = @purchase, UpdatedBy = @by, UpdatedDate = @tgl WHERE ID = @id",
+                new{usage = obj.UomUsage, purchase = obj.UomPurchase, UpdatedBy = obj.CreatedBy, UpdatedDate = DateTime.Now, id = obj.ID}, transaction:Transaction);
         }
     }
 }
