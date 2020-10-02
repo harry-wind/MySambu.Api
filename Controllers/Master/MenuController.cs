7241b729d767730bdafa72f74e525581ae9eb0fe
aerofanz <55221937+aerofanz@users.noreply.github.com>
2020-10-02 10:01:11 +0700

    Update Menu Controller

---
+++ Controllers/Master/MenuController.cs
@@ -0,0 +1,51 @@
+using System.Threading.Tasks;
+using log4net;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySambu.Api.Models;
+using MySambu.Api.Repositorys.Interfaces;
+
+namespace MySambu.Api.Controllers.Master
+{
+    [Route("apimysambu/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+         private static readonly ILog _log = LogManager.GetLogger(typeof(SupplierController));
+        private readonly IUnitOfWorks _uow;
+        private readonly IConfiguration _config;
+        private IHttpContextAccessor _httpContext;
+        public MenuController(IUnitOfWorks uow, IConfiguration config)
+        {
+            _uow = uow;
+            _config = config;
+            // _log = log;
+            _httpContext = (IHttpContextAccessor)new HttpContextAccessor();
+        }
+
+        [AllowAnonymous]
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll(){
+            try
+            {
+                var dt = await _uow.MenuRepository.GetAll();
+                _uow.Commit();
+               
+                var st = StTrans.SetSt(200, 0, "Succes");
+                _log.Info("Get Data Supplier");
+                return Ok(new{Status = st, Results = dt});
+            }
+            catch (System.Exception e)
+            {
+                var st = StTrans.SetSt(400, 0, e.Message);
+                _uow.Rollback();
+                // log4net.LogicalThreadContext.Properties["User"] = sup.CreatedBy;
+                _log.Error("Error : ", e);
+                return Ok(new{Status = st});
+            }
+        }
+
+    }
+}
