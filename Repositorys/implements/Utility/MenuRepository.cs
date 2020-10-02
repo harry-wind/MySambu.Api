1e664d966d4b350ee763bc40391dff494187578a
aerofanz <55221937+aerofanz@users.noreply.github.com>
2020-10-02 10:11:10 +0700

    update menu repo

--- Repositorys/implements/Utility/MenuRepository.cs
+++ Repositorys/implements/Utility/MenuRepository.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
 using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using MySambu.Api.Models.Utility;
 using MySambu.Api.Repositorys.Interfaces;
 
 namespace MySambu.Api.Repositorys.implements
@@ -9,6 +14,36 @@ namespace MySambu.Api.Repositorys.implements
         public MenuRepository(IDbTransaction transaction) : base(transaction)
         {
             
+        }
+
+        public Task Delete(Menu obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Delete(string id, string by)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Menu>> GetAll()
+        {
+            return await Connection.GetAllAsync<Menu>(transaction:Transaction);
+        }
+
+        public Task<Menu> GetByID(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Save(Menu obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Update(Menu obj)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
