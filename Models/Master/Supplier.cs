950d627202bb93922197c07176d37bb016297aa6
aerofanz <55221937+aerofanz@users.noreply.github.com>
2020-10-03 10:18:15 +0700

    update model supplier

--- Models/Master/Supplier.cs
+++ Models/Master/Supplier.cs
@@ -7,19 +7,19 @@ namespace MySambu.Api.Models.Master
     public class Supplier
     {
         [ExplicitKey]
-        public string SupplierID { get; set; }
+       public string SupplierId { get; set; }
         public bool? IsActive { get; set; }
         public string SupplierName { get; set; }
         public string SupplierShortName { get; set; }
         public string AccountNumber { get; set; }
         public string AccountName { get; set; }
-        public int? COAID { get; set; }
-        public string NoCOA { get; set; }
-        public string NoCOAPiutang { get; set; }
+        public int? COAId { get; set; }
+        public string COANo { get; set; }
+        public string COAAccRecNo { get; set; }
         public string GroupWil { get; set; }
         public string GroupDept { get; set; }
         public string SupplierNameChinese { get; set; }
-        public string CountryID { get; set; }
+        public string CountryId { get; set; }
         public bool Import { get; set; }
         public string Address1 { get; set; }
         public string Address2 { get; set; }
@@ -45,14 +45,14 @@ namespace MySambu.Api.Models.Master
         public string Remark { get; set; }
         public string CreatedBy { get; set; }
         public DateTime CreatedDate { get; set; }
-        public string LastUpdatedBy { get; set; }
-        public DateTime? LastUpdatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
         public string Computer { get; set; }
         public DateTime ComputerDate { get; set; }
-        public string SupplierIDOld2 { get; set; }
-        public string SupplierIDOld { get; set; }
+        public string SupplierIdOld2 { get; set; }
+        public string SupplierIdOld { get; set; }
         public bool Modified { get; set; }
         public bool Register { get; set; }
-        public string JenisSupplier { get; set; }
+        public string SupplierType { get; set; }
     }
 }
