a27920f77d1af8223d73d49595e5c87bc711c38e
aerofanz <55221937+aerofanz@users.noreply.github.com>
2020-10-05 17:06:16 +0700

    init item

---
+++ Controllers/Master/ItemNewController.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using log4net;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySambu.Api.Models;
+using MySambu.Api.Models.Master;
+using MySambu.Api.Repositorys.Interfaces;
+
+namespace MySambu.Api.Controllers.Master
+{
+    [Route("apimysambu/[controller]")]
+    [ApiController]
+    public class ItemNewController : ControllerBase
+    {
+         private static readonly ILog _log = LogManager.GetLogger(typeof(ItemNewController));
+        private readonly IUnitOfWorks _uow;
+        private readonly IConfiguration _config;
+        private IHttpContextAccessor _httpContext;
+        public ItemNewController(IUnitOfWorks uow, IConfiguration config)
+        {
+            _uow = uow;
+            _config = config;
+            _httpContext = (IHttpContextAccessor)new HttpContextAccessor();
+        }
+        
+        [Authorize(Policy="RequireAdmin")]        
+        [HttpPost("Save")]
+        public async Task<IActionResult> Save(BudgetCategory dt){
+            string userby = _httpContext.HttpContext.User.FindFirst(ClaimTypes.Name).Value;
+            try
+            {
+                dt.BudgetCategoryID = 0;
+                dt.CreatedBy = userby;
+                dt.CreatedDate = DateTime.Now;
+                await _uow.BudgetCategoryRepository.Save(dt);
+               
+                _uow.Commit();
+
+                log4net.LogicalThreadContext.Properties["NewValue"] = Logs.ToJson<BudgetCategory>(dt);
+                log4net.LogicalThreadContext.Properties["User"] = userby;
+                _log.Info("Succes Save");
+                
+                var st = StTrans.SetSt(200, 0, "Succes");
+                return Ok(new{Status = st, Results = dt});
+        
+            }
+            catch (System.Exception e)
+            {
+                var st = StTrans.SetSt(400, 0, e.Message);
+                _uow.Rollback();
+
+                log4net.LogicalThreadContext.Properties["NewValue"] = Logs.ToJson<BudgetCategory>(dt);
+                log4net.LogicalThreadContext.Properties["User"] = userby;
+                _log.Error("Error : ", e);
+                return Ok(new{Status = st});
+            }
+        }
+
+
+        [Authorize(Policy="RequireAdmin")]
+        [HttpPost("Update")]
+        public async Task<IActionResult> UpdatedCountry(BudgetCategory dt){
+            string userby = _httpContext.HttpContext.User.FindFirst(ClaimTypes.Name).Value;
+            try
+            {
+                dt.CreatedBy = userby;
+                dt.CreatedDate = DateTime.Now;
+                await _uow.BudgetCategoryRepository.Save(dt);
+               
+                _uow.Commit();
+
+                log4net.LogicalThreadContext.Properties["NewValue"] = Logs.ToJson<BudgetCategory>(dt);
+                log4net.LogicalThreadContext.Properties["User"] = userby;
+                _log.Info("Succes Save");
+                
+                
+                var st = StTrans.SetSt(200, 0, "Succes");
+                return Ok(new{Status = st, Results = dt});
+        
+            }
+            catch (System.Exception e)
+            {
+                var st = StTrans.SetSt(400, 0, e.Message);
+                _uow.Rollback();
+
+                log4net.LogicalThreadContext.Properties["NewValue"] = Logs.ToJson<BudgetCategory>(dt);
+                log4net.LogicalThreadContext.Properties["User"] = userby;
+                _log.Error("Error : ", e);
+                return Ok(new{Status = st});
+            }
+        }
+
+        [Authorize(Policy="RequireAdmin")]
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll(){
+            string userby = _httpContext.HttpContext.User.FindFirst(ClaimTypes.Name).Value;
+            try
+            {
+                var dt = await _uow.ItemNewRepository.GetAll();
+                _uow.Commit();
+               
+                var st = StTrans.SetSt(200, 0, "Succes");
+                return Ok(new{Status = st, Results = dt});
+            }
+            catch (System.Exception e)
+            {
+                var st = StTrans.SetSt(400, 0, e.Message);
+                _uow.Rollback();
+                log4net.LogicalThreadContext.Properties["User"] = userby;
+                _log.Error("Error : ", e);
+                return Ok(new{Status = st});
+            }
+
+        }
+    }
+}
