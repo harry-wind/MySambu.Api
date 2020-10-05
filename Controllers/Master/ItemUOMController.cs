b555d6e2980b82f555798d9e062487a1f4fa882b
aerofanz <55221937+aerofanz@users.noreply.github.com>
2020-10-05 17:06:50 +0700

    update item uom

--- Controllers/Master/ItemUOMController.cs
+++ Controllers/Master/ItemUOMController.cs
@@ -29,16 +29,17 @@ namespace MySambu.Api.Controllers.Master
         
         [Authorize(Policy="RequireAdmin")]        
         [HttpPost("Save")]
-        public async Task<IActionResult> Save(Country count){
+        public async Task<IActionResult> Save(ItemUOM count){
             string userby = _httpContext.HttpContext.User.FindFirst(ClaimTypes.Name).Value;
             try
             {
-                count.CountryId = _uow.GetGUID();
+                
+                count.CreatedBy = userby;
                 count.CreatedDate = DateTime.Now;
-                await _uow.CountryRepository.Save(count);
-               
+                await _uow.ItemUOMRepository.Save(count);
                 _uow.Commit();
 
+                log4net.LogicalThreadContext.Properties["NewValue"] = Logs.ToJson<ItemUOM>(count);
                 log4net.LogicalThreadContext.Properties["User"] = userby;
                 _log.Info("Succes Save");
                 
@@ -51,6 +52,7 @@ namespace MySambu.Api.Controllers.Master
                 var st = StTrans.SetSt(400, 0, e.Message);
                 _uow.Rollback();
 
+                log4net.LogicalThreadContext.Properties["NewValue"] = Logs.ToJson<ItemUOM>(count);
                 log4net.LogicalThreadContext.Properties["User"] = userby;
                 _log.Error("Error : ", e);
                 return Ok(new{Status = st});
@@ -60,15 +62,19 @@ namespace MySambu.Api.Controllers.Master
 
         [Authorize(Policy="RequireAdmin")]
         [HttpPost("Update")]
-        public async Task<IActionResult> UpdatedCountry(Country count){
+        public async Task<IActionResult> UpdatedCountry(ItemUOM count){
+            string userby = _httpContext.HttpContext.User.FindFirst(ClaimTypes.Name).Value;
             try
             {
-                // count.CountryId = _uow.GetGUID();
+                
+                count.CreatedBy = userby;
                 count.CreatedDate = DateTime.Now;
-                await _uow.CountryRepository.Save(count);
-               
+                await _uow.ItemUOMRepository.Save(count);
                 _uow.Commit();
-                _log.Info("Succes Updated");
+
+                log4net.LogicalThreadContext.Properties["NewValue"] = Logs.ToJson<ItemUOM>(count);
+                log4net.LogicalThreadContext.Properties["User"] = userby;
+                _log.Info("Succes Update");
                 
                 var st = StTrans.SetSt(200, 0, "Succes");
                 return Ok(new{Status = st, Results = count});
@@ -79,7 +85,8 @@ namespace MySambu.Api.Controllers.Master
                 var st = StTrans.SetSt(400, 0, e.Message);
                 _uow.Rollback();
 
-                log4net.LogicalThreadContext.Properties["User"] = count.CreatedBy;
+                log4net.LogicalThreadContext.Properties["NewValue"] = Logs.ToJson<ItemUOM>(count);
+                log4net.LogicalThreadContext.Properties["User"] = userby;
                 _log.Error("Error : ", e);
                 return Ok(new{Status = st});
             }
@@ -105,7 +112,28 @@ namespace MySambu.Api.Controllers.Master
                 _log.Error("Error : ", e);
                 return Ok(new{Status = st});
             }
+        }
 
+        [Authorize(Policy="RequireAdmin")]
+        [HttpGet("GetByStatus")]
+        public async Task<IActionResult> GetByStatus(bool IsActive){
+            string userby = _httpContext.HttpContext.User.FindFirst(ClaimTypes.Name).Value;
+            try
+            {
+                var dt = await _uow.ItemUOMRepository.GetByStatus(IsActive);
+                _uow.Commit();
+               
+                var st = StTrans.SetSt(200, 0, "Succes");
+                return Ok(new{Status = st, Results = dt});
+            }
+            catch (System.Exception e)
+            {
+                var st = StTrans.SetSt(400, 0, e.Message);
+                _uow.Rollback();
+                log4net.LogicalThreadContext.Properties["User"] = userby;
+                _log.Error("Error : ", e);
+                return Ok(new{Status = st});
+            }
         }
     }
 }
