52640d0d780f765af34104af2efae4b53b85d4c6
Harry Windharto <harrywindharto@gmail.com>
2020-10-09 16:19:37 +0700

    Add Dapper Contrib di Structure Level

--- Models/Master/StructureLevelModel.cs
+++ Models/Master/StructureLevelModel.cs
@@ -1,7 +1,11 @@
+using Dapper.Contrib.Extensions;
+
 namespace MySambu.Api.Models.Master
 {
+    [Table("tMst_StructureLevel")]
     public class StructureLevelModel
     {
+        [ExplicitKey]
         public int StructureLevel { get; set; }
         public string StructureRemark { get; set; }
     }
